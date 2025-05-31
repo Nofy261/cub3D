bcc6bbe34caed8611bbdae8158699b201db02e12
Nofy Leclercq <nolecler@1G7.42angouleme.fr>
2025-05-31 12:08:39 +0200

    Color parsing is correct except it fails when there are spaces after the last digit

--- src/parsing/parse_colors.c
+++ src/parsing/parse_colors.c
@@ -6,24 +6,41 @@
 /*   By: nolecler <nolecler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 08:12:57 by nolecler          #+#    #+#             */
-/*   Updated: 2025/05/28 08:41:40 by nolecler         ###   ########.fr       */
+/*   Updated: 2025/05/31 12:00:30 by nolecler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-static int	is_valid_color(char *rgb)
+// ne pas avoir d'alphabet dans la couleur ✅
+// gerer les espaces-tab entre les couleurs EX: "255,  89, 22"
+// "2  55,240,6  6" ✅
+// espaces apres le dernier chiffres a gerer comme pas erreur ❌
+// ignorer le + et le - ✅
+// espace apres les chiffres c est gerer mais espaces avant les chiffres non gerer✅
+// verifier si les couleurs commence ou termine par une virgule : erreur ✅
+
+
+static int	is_valid_color(char *rgb, t_data *data)
 {
 	int i;
-    
+    int count_comma;
+
     i = 0;
+    count_comma = 0;
 	while (rgb[i])
 	{
-		if (!ft_isdigit(rgb[i]) && rgb[i] != ',')
-			return (0);
-        // gere le cas de double virgule (224,,122,0)
-		if (rgb[i] == ',' && (i == 0 || !ft_isdigit(rgb[i - 1]) || !ft_isdigit(rgb[i + 1])))
-			return (0);
+        if (rgb[i] == ',')
+            count_comma++;
+        if (count_comma > 2 || (is_whitespace(rgb[i]) == 1) || (!ft_isdigit(rgb[i]) && rgb[i] != ',') ||
+            (rgb[i] == ',' && is_whitespace(rgb[i - 1]) == 1) ||
+            (rgb[i] == ',' && is_whitespace(rgb[i + 1]) == 1))
+        {
+            ft_putstr_fd("Error\n", 2);
+            ft_putstr_fd("invalid color format 1\n", 2);
+            free_data(data);
+            exit(EXIT_FAILURE);
+        }
 		i++;
 	}
 	return (1);
@@ -37,7 +54,8 @@ static int	convert_and_check_rgb(char **colors, int *r, int *g, int *b)
     *b = ft_atoi(colors[2]);
     if (*r < 0 || *r > 255 || *g < 0 || *g > 255 || *b < 0 || *b > 255)
     {
-        ft_putstr_fd("Error: Color value is invalid\n", 2);
+        ft_putstr_fd("Error\n", 2);
+        ft_putstr_fd("Color value is invalid\n", 2);
         free_array(colors);
         return (0);
     }
@@ -59,17 +77,21 @@ static void parse_color_line(t_data *data, char *rgb, int is_floor)
     int g;
     int b;
 
-    if (!is_valid_color(rgb))
-        return; 
+    is_valid_color(rgb, data);
     colors = ft_split(rgb, ','); // on split "220,100,0"
     if (!colors || !colors[0] || !colors[1] || !colors[2] || colors[3])
     {
-        ft_putstr_fd("Error: Invalid color format\n", 2);// plus de 3couleurs
+        ft_putstr_fd("Error\n", 2);
+        ft_putstr_fd("Invalid color format\n", 2);
         free_array(colors);
-        return;
+        free_data(data);
+        exit(EXIT_FAILURE);
     }
     if (convert_and_check_rgb(colors, &r, &g, &b) == 0)
-        return ;
+    {
+        free_data(data);
+        exit(EXIT_FAILURE);
+    }
     if (is_floor)
         set_color(data->map->floor_color, r, g, b);
     else
